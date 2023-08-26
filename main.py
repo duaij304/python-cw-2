703e306153464640db7f7661b861f44c239571f1
duaij304 <duaijshadeed@gmail.com>
2023-08-26 18:27:37 +0300

    test

---
+++ main.py
@@ -0,0 +1,22 @@
+my_name = input("what is your name")
+my_age= input ("how old are you")
+
+print(f"My name is {my_name} and I am {my_age} years old")
+
+first_number= int( input ("first number "))
+secound_number= int( input(" second number "))
+
+operation= input ("what you want to chose, (*,+,-,/)")
+
+if operation =="*":
+    print(first_number*secound_number) 
+elif operation =="+":
+        print(first_number+secound_number) 
+
+
+elif operation =="/":
+ print(first_number/secound_number) 
+
+ 
+elif operation =="-":
+            print(first_number-secound_number) 
